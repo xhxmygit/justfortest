4af1f8708069224829e95527c7d7ad67fb2ef2d5
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2016-10-12 17:38:05 +0800

    Just for test

---
+++ CI_BIS.ps1
@@ -0,0 +1,17 @@
+
+echo "------------------------------------"
+$vmName = "FreeBSD11"
+$hvServer = "localhost"
+
+# $v = Get-VM  -Name $vmName -ComputerName $hvServer
+$VMs = Get-VM 
+if ($VMs -eq $null)
+{
+	"Error: VM cannot find the VMs"
+	return 1
+} 
+
+
+"The VMs are: $VMs"
+echo "------------------------------------"
+
