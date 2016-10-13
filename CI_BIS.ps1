80e7816ce308ee20c512ac255dcf9730336e7596
Hongxiong Xian (Wicresoft) <v-hoxian@microsoft.com>
2016-10-13 09:21:10 +0800

    Just for test

--- CI_BIS.ps1
+++ CI_BIS.ps1
@@ -1,5 +1,28 @@
 
 echo "------------------------------------"
+
+# SetExecutionPolicyToRemoteSigned.ps1
+# Need to run this after every server reboot.
+Write-Output "Setting local Powershell policy to RemoteSigned"
+Write-Output ""
+
+Set-ExecutionPolicy -scope CurrentUser Undefined -Force
+#Set-ExecutionPolicy -scope Process Undefined -Force
+Set-ExecutionPolicy -scope LocalMachine Undefined -Force
+
+Set-ExecutionPolicy -scope CurrentUser RemoteSigned -Force
+#Set-ExecutionPolicy -scope Process RemoteSigned -Force
+Set-ExecutionPolicy -scope LocalMachine RemoteSigned -Force
+
+Write-Output "Finished."
+
+Get-ExecutionPolicy -list
+Start-Sleep -s 10
+
+echo "------------------------------------"
+
+
+
 $vmName = "FreeBSD11"
 $hvServer = "localhost"
 
